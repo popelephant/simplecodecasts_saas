4222a69900e3da08175b2633b0382bc7ccad7fec
AV <aerovero@gmail.com>
2016-04-12 21:57:19 +0000

    Installed devise

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.stmp_settings = {
+ActionMailer::Base.smtp_settings = {
     :address => 'stmp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com'
-    :enable_startstls_auto => true,
-}
+    :domain => 'heroku.com',
+    :enable_startstls_auto => true }
